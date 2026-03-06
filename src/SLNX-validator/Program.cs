c5639e99800dbe5a24b7ca29dc50d6b4be5bb3bc
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2026-03-06 22:22:45 +0100

    Remove ValidationReporter from service registration
    Removed ValidationReporter service registration from the service collection.

--- src/SLNX-validator/Program.cs
+++ src/SLNX-validator/Program.cs
@@ -21,7 +21,6 @@ public static class Program
         var services = new ServiceCollection()
             .AddSlnxValidator()
             .AddSingleton<ValidationCollector>()
-            .AddSingleton<ValidationReporter>()
             .AddSingleton<ValidatorRunner>()
             .BuildServiceProvider();
 
